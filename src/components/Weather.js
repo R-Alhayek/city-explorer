85809cff171b2a8f46ca627348daf85aa7305f8c
‘R-Alhayek’ <rafeefalhayek94@outlook.com>
2021-07-06 19:46:32 +0300

    finished lab7

---
+++ src/components/Weather.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+
+
+
+class Weather extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>
+
+                    {this.props.forcast.map(day => {
+                        return <div> <Card style={{ width: '18rem' }}>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>{day.date} {day.description}</ListGroup.Item>
+
+                            </ListGroup>
+                        </Card> </div>
+                    })}
+                </div>
+
+            </div>
+        )
+    }
+
+}
+export default Weather;
